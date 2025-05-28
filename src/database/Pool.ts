bf2d4a9429798a0609b246c0c72a1679f34df952
Luiz Felipe <felipe.pinghang2@gmail.com>
2025-05-28 16:38:57 -0300

    feat: adicionando arquivos do professor

---
+++ src/database/Pool.ts
@@ -0,0 +1,9 @@
+import { Pool } from "pg";
+
+export default new Pool({
+  host: process.env.BD_HOST,
+  user: process.env.BD_USER,
+  password: process.env.BD_PASSWORD,
+  database: process.env.BD_DATABASE,
+  port: Number(process.env.BD_PORT)
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import exampleRoutes from './routes/example.routes';
+import dotenv from "dotenv";
+import cidadesRoutes from './routes/cidade.routes';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001
@@ -8,7 +11,7 @@ const port = process.env.PORT || 3001
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', exampleRoutes)
+app.use('/api', cidadesRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
